dd1640a908fbeaf1036332f3d39f369b6273fa9e
Daniel Le Jeune <LJNDAN001@myuct.ac.za>
2026-05-21 11:57:31 +0200

    ✨ Added progress bar

--- src/vm_cleanup/inventory.py
+++ src/vm_cleanup/inventory.py
@@ -1,6 +1,6 @@
 import os
 import pwd
-
+from alive_progress import alive_bar
 from collections import defaultdict
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -32,46 +32,48 @@ def audit(data_dir: Path, out_dir: Path, stale_days: int, min_size_mb: int):
     total_files = 0
 
     logger.info("Starting Walkthrough")
-
-    for root, _, files in os.walk(data_dir):
-        for fname in files:
-            total_files += 1
-            fpath = Path(root) / fname
-            try:
-                st = fpath.stat()
-                size = fpath.stat().st_size
-
-                uid = st.st_uid
+    with alive_bar() as bar:
+        for root, _, files in os.walk(data_dir):
+            for fname in files:
+                total_files += 1
+                fpath = Path(root) / fname
                 try:
-                    owner = pwd.getpwuid(uid).pw_name
-                except KeyError:
-                    owner = str(uid)
+                    st = fpath.stat()
+                    size = fpath.stat().st_size
 
-                ext = fpath.suffix.lstrip(".").lower() or "no_extension"
-                mtime = datetime.fromtimestamp(st.st_mtime)
+                    uid = st.st_uid
+                    try:
+                        owner = pwd.getpwuid(uid).pw_name
+                    except KeyError:
+                        owner = str(uid)
 
-                owner_size_map[owner] += size
-                owner_count_map[owner] += 1
+                    ext = fpath.suffix.lstrip(".").lower() or "no_extension"
+                    mtime = datetime.fromtimestamp(st.st_mtime)
 
-                file_type_size_map[ext] += size
-                file_type_count_map[ext] += 1
+                    owner_size_map[owner] += size
+                    owner_count_map[owner] += 1
 
-                if mtime < cutoff:
-                    stale_files.append(
-                        StaleFile(
-                            last_modified=mtime.strftime("%Y-%m-%d"),
-                            size_bytes=str(size),
-                            path=str(fpath),
+                    file_type_size_map[ext] += size
+                    file_type_count_map[ext] += 1
+
+                    if mtime < cutoff:
+                        stale_files.append(
+                            StaleFile(
+                                last_modified=mtime.strftime("%Y-%m-%d"),
+                                size_bytes=str(size),
+                                path=str(fpath),
+                            )
                         )
-                    )
 
-                if size >= min_bytes:
-                    large_files.append(
-                        LargeFile(extension=ext, size_bytes=size, path=str(fpath))
-                    )
+                    if size >= min_bytes:
+                        large_files.append(
+                            LargeFile(extension=ext, size_bytes=size, path=str(fpath))
+                        )
 
-            except OSError as os_error:
-                logger.error(os_error)
+                except OSError as os_error:
+                    logger.error(os_error)
+
+                bar()
 
     logger.info("Writing outputs")
     owner_size_output = out_dir / "owner_size.tsv"
